a29a7b7552e398ca67454ddcacd1e37de94101e1
garud-1 <akswale111@gmail.com>
2024-10-04 18:27:06 +0530

    added sample video

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import WhoWeAre from "../../components/whoWeare/WhoWeAre";
 import MustSee from "../../components/MustSee/MustSee";
 import TripCard from "@/components/TripCard/TripCard";
 import Slider from "@/components/Slider/Slider";
-import Upcoming from "../../components/Upcoming/Upcoming"
+import Upcoming from "../../components/Upcoming/Upcoming";
 import UpComingTripCard from "@/components/UpComingTripCard/UpComingTripCard";
 import UpcomingSlider from "@/components/Slider/UpcomingSlider";
 
@@ -12,18 +12,37 @@ function Home() {
   return (
     <div>
       {/* main video or image */}
+      {/* <div className="mainPhotoContainer"> */}
+      {/* <img src="stargazing.jpg" alt="stargazing" /> */}
+      {/* <video  src="https://www.incredibleindia.gov.in/content/dam/incredible-india/videos/home/India-360-v2.mp4"  alt ="ye na cholbe"></video> */}
+      {/* <iframe src="https://www.incredibleindia.gov.in/content/dam/incredible-india/videos/home/India-360-v2.mp4" frameborder="0">Vedio</iframe> */}
+      {/* </div> */}
       <div className="mainPhotoContainer">
-        <img src="stargazing.jpg" alt="stargazing" />
+        <video
+          width="100%"
+          height="auto"
+          autoplay
+          muted
+          loop
+          controlsList="nodownload nofullscreen noremoteplayback"
+          disablePictureInPicture
+        >
+          <source
+            src="https://www.incredibleindia.gov.in/content/dam/incredible-india/videos/home/India-360-v2.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+        {/* </div>   */}
       </div>
 
       <WhoWeAre />
       {/* <TripCard /> */}
       <MustSee />
-      <Slider/>
-      <Upcoming/>
+      <Slider />
+      <Upcoming />
       {/* <UpComingTripCard/> */}
-      <UpcomingSlider/>
-      
+      <UpcomingSlider />
     </div>
   );
 }
